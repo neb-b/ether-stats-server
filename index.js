750f06c8d8b08b078d5fd3a730c89d3f95d580d0
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-05-23 16:42:00 -0700

    simple payload formating

--- index.js
+++ index.js
@@ -8,18 +8,36 @@ const ROOT_URL = 'https://ethermine.org/api/miner_new'
 app.use(cors())
 app.get('/favicon.ico', (req, res) => res.sendStatus(200))
 
+const formatPayload = (payload) => {
+  const avgHashrate = payload.avgHashrate
+  const megaHashrate = (avgHashrate / 1000000).toFixed(1)
+  let dayAvgHashrate = megaHashrate < 10 ? `0${megaHashrate}` : `${megaHashrate}`
+  dayAvgHashrate += 'MH/s'
+
+  return Object.assign({}, payload, { avgHashrate: dayAvgHashrate })
+}
+
 app.get('/:wallet', (req, res) => {
   const wallet = req.params.wallet
-  const url = `${ROOT_URL}/${wallet}`
 
-  return axios.get(url)
+  const checkWallet = (str) => /^[a-zA-Z0-9]{40}$/.test(str)
+  const isCorrectFormat = checkWallet(wallet)
+
+  if (isCorrectFormat) {
+    const url = `${ROOT_URL}/${wallet}`
+
+    return axios.get(url)
     .then((response) => {
       const data = response.data
-      res.send(data)
+      const formattedPayload = formatPayload(data)
+      res.send(formattedPayload)
     })
     .catch((err) => {
       res.send(err)
     })
+  } else {
+    res.send('Invalid wallet').sendStatus(400)
+  }
 })
 
 app.listen(PORT)
